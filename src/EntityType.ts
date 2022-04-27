328cdd449e1ba5fbf9b81b84aecf665eeed3dc0f
shinobu-takahashi <shinobu_takahashi@c.dwango.co.jp>
2022-04-27 16:40:18 +0900

    use enum

--- src/EntityType.ts
+++ src/EntityType.ts
@@ -1,14 +1,12 @@
 //
 // Entity識別子定義
 //
-export const EntityType = {
-    PLAYER: 0,
-    ENEMY: 1,
-    PLAYER_BULLET: 2,
-    ENEMY_BULLET: 3,
-    EFFECT: 4,
-    ITEM: 5,
-    OBSTACLE: 6
-} as const;
-
-export type EntityType = typeof EntityType[keyof typeof EntityType];
+export enum EntityType {
+	PLAYER,
+	ENEMY,
+	PLAYER_BULLET,
+	ENEMY_BULLET,
+	EFFECT,
+	ITEM,
+	OBSTACLE
+}
--- src/ItemType.ts
+++ src/ItemType.ts
@@ -1,13 +1,11 @@
 //
 // アイテム識別子定義
 //
-export const ItemType = {
-    SHIELD: 0,
-    HOMING: 1,
-    RAPIDFIRE: 2,
-    BULLETSPEED: 3,
-    PIERCING: 4,
-    RECOVER: 5
-} as const;
-
-export type ItemType = typeof ItemType[keyof typeof ItemType];
+export enum ItemType {
+	SHIELD,
+	HOMING,
+	RAPIDFIRE,
+	BULLETSPEED,
+	PIERCING,
+	RECOVER
+}
